25d0297de92a244c2795dda7b7d86ca60842e4e9
Harisali78 <aliharis2739@gmail.com>
2023-01-05 13:36:49 +0500

    button alignment

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <button>Get Started</button>
       </div>
     </>
